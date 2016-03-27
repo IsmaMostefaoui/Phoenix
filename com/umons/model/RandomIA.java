a5897467dbf27fbc69935776a5cd18440e78f7eb
Robin_C <robchoquet@hotmail.com>
2016-03-27 21:06:12 +0200

    Debut IARandom

--- com/umons/model/RandomIA.java
+++ com/umons/model/RandomIA.java
@@ -7,13 +7,18 @@ public class RandomIA extends Player {
 		super(loc, OrderNumber);	
 	}
 	
-	public static boolean move(Grid grid) {
+	public boolean move(Grid grid, Player player) {
 		Random rand = new Random();
 		boolean choix= rand.nextBoolean();
 		if (choix == true) {
-			if (ARules.rMovePion(Player, loc.squareUP)){ //comment instanceier un player et une loc ????  #TODO
+			if (ARules.rMovePion(player, this.getLoc().squareDown())) { 
+				this.setLoc(this.getLoc().squareDown());
+				grid.setItemInGrid(this.getLoc(), false);
+				grid.setItemInGrid(this.getLoc().squareDown(), true);
+				return true;
 			}	
 		}
+		return false;
 	}
 
 }
