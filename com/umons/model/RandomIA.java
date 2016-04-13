13196ac28b8e3493d0757219bd159c7b8bcc3fd9
Robin_C <robchoquet@hotmail.com>
2016-04-13 16:52:30 +0200

    Ajout de la RandomIA

--- com/umons/model/RandomIA.java
+++ com/umons/model/RandomIA.java
@@ -1,24 +1,72 @@
 package com.umons.model;
+import java.util.List;
 import java.util.Random;
 
 public class RandomIA extends Player {
 	
-	public RandomIA(Location loc, int OrderNumber) {
-		super(loc, OrderNumber);	
+	
+	public RandomIA(Location loc, int NbreOfWall ,int OrderNumber) {
+		super(loc, NbreOfWall, OrderNumber);	
 	}
 	
-	public boolean move(Grid grid, Player player) {
+	/**
+	 * Choisis aleatoirement entre se deplacer et poser un mur, si impossible de placer un mur -> se deplace
+	 * @param grid palteau de jeu
+	 * @param opponent Instance Player du joueur adverse
+	 */
+	public void play(Grid grid, Player opponent) {
 		Random rand = new Random();
-		boolean choix= rand.nextBoolean();
-		if (choix == true) {
-			if (ARules.rMovePion(player, this.getLoc().squareDown())) { 
-				this.setLoc(this.getLoc().squareDown());
-				grid.setItemInGrid(this.getLoc(), false);
-				grid.setItemInGrid(this.getLoc().squareDown(), true);
+		boolean choice = rand.nextBoolean();
+		if (choice == true) {
+			this.move(grid);
+		}
+		if (choice == false) {
+			if (!this.putwall(grid, opponent)) { // si ne peut poser aucun mur, faire un deplacement
+				this.move(grid);
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * L'IA choissit la position la plus proche de la ligne d'arrivée
+	 * @param grid Grille de jeu
+	 */
+	public void move(Grid grid) {
+		List<Location> list = ARules.rSquareAvailable(this);
+		Location nextLocation = this.getLoc();
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getLocY() >= nextLocation.getLocY()) {
+				nextLocation = list.get(i);	
+			}
+		}
+		grid.setItemInGrid(this.getLoc(), false);
+		grid.setItemInGrid(nextLocation, true);
+		this.setLoc(nextLocation);
+
+	}
+	
+	
+	/**
+	 * Pose un mur autour du Pion adverse si possible (ordre : up,left, right, down)
+	 * @param grid plateau de jeu
+	 * @param opponent Instance Player du joueur adverse
+	 * @return true si il peut poser un mur, sinon false
+	 */
+	public boolean putwall(Grid grid, Player opponent) {
+		Location[] tabWall = {opponent.getLoc().wallUp(), opponent.getLoc().wallLeft(), opponent.getLoc().wallRight(), opponent.getLoc().wallDown() };
+		int numb = 0;
+		while (numb < 4) {
+			if (super.putWall(grid, tabWall[numb])) {
 				return true;
-			}	
+			}
+			numb++;
 		}
 		return false;
 	}
+	
+	
 
 }
+
