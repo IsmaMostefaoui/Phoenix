247d287a20792133f2a7363d1d695098921a4d26
isma <ismajesuis@live.fr>
2016-04-27 20:28:11 +0200

    Ajout JMenuBar (pour undo et choix mode et etc...)

--- com/umons/view/QuoridorGUI.java
+++ com/umons/view/QuoridorGUI.java
@@ -5,11 +5,17 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;	
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+
 public class QuoridorGUI extends JFrame{
+	
+	boolean printMenuBar = false;
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -27,20 +33,17 @@ public class QuoridorGUI extends JFrame{
 	public static int BOARDGUI = 0;
 	public static int MENUGUI = 1;
 	public static int RULESGUI = 2;
+	public static int VICTORYGUI = 3;
 	
 	private ArrayList<JPanel> nextPanes = new ArrayList<JPanel>();
 	
-	public QuoridorGUI(String title, JPanel...panels) {
+	public QuoridorGUI(String title) {
 		super();
 		setTitle(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setSize(WIDTH, HEIGHT);
 		setLocationRelativeTo(null);
-		
-		for (int i = 0; i < panels.length; i++) {
-			nextPanes.add(panels[i]);
-		}
 		
 		setVisible(true);
 	}
@@ -49,10 +52,57 @@ public class QuoridorGUI extends JFrame{
 		getContentPane().removeAll();
 		System.out.println(nextPanes.get(panelName));
 		setContentPane(nextPanes.get(panelName));
+		nextPanes.get(panelName).repaint();
+		if (panelName == BOARDGUI) {
+			printMenuBar = true;
+			menuBar();
+		}else {
+			printMenuBar = false;
+		}
 		SwingUtilities.updateComponentTreeUI(this);
+		nextPanes.get(panelName).repaint();
+		
 	}
 	
 	public void setPane(JPanel panelToAdd) {
 		nextPanes.add(panelToAdd);
+		System.out.println(nextPanes.size());
+	}
+	
+	public void menuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		if (printMenuBar) {
+			this.setJMenuBar(menuBar);
+
+			JMenu menuPartie = new JMenu("Partie");
+			menuBar.add(menuPartie);
+
+			JMenuItem partie = new JMenuItem("Choix Mode");
+			JMenuItem start = new JMenuItem("Exit");
+			JMenuItem stop = new JMenuItem("Undo (Ctrl+Z");
+
+			menuPartie.add(partie); 
+			menuPartie.add(start); 
+			menuPartie.add(stop);
+
+			JMenu menuCouleur = new JMenu("Couleur");
+			menuBar.add(menuCouleur);
+
+			JMenu snake = new JMenu("Snake");
+			JMenu wall = new JMenu("Wall");
+
+			menuCouleur.add(snake); menuCouleur.add(wall);
+
+			JMenuItem noirS = new JMenuItem("Noir (banane au miel, KFC, et toutes les epices)");
+			JMenuItem arabeS = new JMenuItem("Couleur Arabe(thé, couscous, tajiin,  etc...)");
+
+			JMenuItem noirW = new JMenuItem("Noir (banane au miel, KFC, et toutes les epices)");
+			JMenuItem arabeW = new JMenuItem("Couleur Arabe(thé, couscous, tajiin,  etc...)");
+
+			snake.add(noirS); snake.add(arabeS);
+			wall.add(noirW); wall.add(arabeW);
+		}else {
+			getContentPane().remove(menuBar);
+		}
 	}
 }
